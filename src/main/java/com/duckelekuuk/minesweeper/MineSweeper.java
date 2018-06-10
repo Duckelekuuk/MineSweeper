55c38735e978002e612e85b755339160b48d2ffe
Duco Lindner <duckelekuuk@gmail.com>
2018-06-10 18:43:10 +0200

    first commit

---
+++ src/main/java/com/duckelekuuk/minesweeper/MineSweeper.java
@@ -0,0 +1,17 @@
+package com.duckelekuuk.minesweeper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineSweeper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
